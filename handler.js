b63e14d60c59031f03b57c4776fdae85bb36f94b
Sebastian Portesi <sportesi@octopus.com.ar>
2018-12-04 15:48:07 -0300

    Test como dice la docu... please...

---
+++ handler.js
@@ -0,0 +1,29 @@
+const chromium = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
+
+exports.handler = async (event, context) => {
+  let result = null;
+  let browser = null;
+
+  try {
+    browser = await puppeteer.launch({
+      args: chromium.args,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+    });
+
+    let page = await browser.newPage();
+
+    await page.goto(event.url || 'https://example.com');
+
+    result = await page.title();
+  } catch (error) {
+    return context.fail(error);
+  } finally {
+    if (browser !== null) {
+      await browser.close();
+    }
+  }
+
+  return context.succeed(result);
+};
