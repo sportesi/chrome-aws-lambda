7f655cd431f7403290a2588c466ec9ecb7a36326
Sebastian Portesi <sportesi@octopus.com.ar>
2019-01-28 12:14:30 -0300

    Refactor para nueva arquitectura (SQS)

--- handler.js
+++ handler.js
@@ -3,7 +3,6 @@ const puppeteer = require('puppeteer-core');
 const AWS = require('aws-sdk');
 const fs = require('fs-extra');
 const { pdfSettings } = require('./src/pdfSettings');
-const uuid4 = require('uuid4');
 
 async function timeout(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -12,9 +11,14 @@ async function timeout(ms) {
 module.exports.handler = async (event, context) => {
   let result = null;
   let browser = null;
+  
+  const message = JSON.parse(event.Records[0].body);
+  
   const s3 = new AWS.S3();
-  const bucket = event.Records[0].s3.bucket.name // Con esto me traigo el bucket a donde tengo que dejar el pdf
-  const jsonPath = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
+  const sqs = new AWS.SQS({ apiVersion: '2012-11-05', region: 'us-west-2' });
+  
+  const bucket = message.bucket;
+  const jsonPath = decodeURIComponent(message.ruta.replace(/\+/g, " "));
 
   try {
 
@@ -29,14 +33,9 @@ module.exports.handler = async (event, context) => {
 
     jsonData = JSON.parse(jsonData.Body.toString());
 
-    // Traigo el html a traves del json
+    // Traigo el html dentro del JSON
 
-    const htmlFileS3Params = {
-      Bucket: bucket,
-      Key: `pdfs/expense-json-to-generate/${jsonData.html_filename}`
-    };
-
-    let html = await s3.getObject(htmlFileS3Params).promise();
+    let html = jsonData.html_expensa;
 
     // Invoco el browser
 
@@ -48,7 +47,7 @@ module.exports.handler = async (event, context) => {
 
     let page = await browser.newPage();
 
-    await page.goto(`data:text/html,${html.Body.toString()}`, {
+    await page.goto(`data:text/html,${html}`, {
       waitUntil: 'networkidle0'
     });
 
@@ -71,83 +70,25 @@ module.exports.handler = async (event, context) => {
     let uploadedPdfParams = {
       Body: fs.readFileSync('/tmp/expense.pdf'),
       Bucket: bucket,
-      Key: jsonData.pdf_path
+      Key: jsonData.ruta
     };
 
     await s3.putObject(uploadedPdfParams).promise();
 
-    // Borro el json
+    let ACQueueUrl = "https://sqs.us-west-2.amazonaws.com/730404845529/qa_account_status_pdf_queue";
 
-    await s3.deleteObject(jsonFileS3Params).promise();
-
-    // Borro el html
-
-    await s3.deleteObject(htmlFileS3Params).promise();
-
-    // Llamo al lambda que va a mergear este pdf con el de account-status
-
-    const lambda = new AWS.Lambda({
-      apiVersion: '2015-03-31',
-      region: 'us-west-2',
-    })
-
-    let lambdaParams = {
-      FunctionName: process.env.MERGER_LAMBDA,
-      InvocationType: 'Event',
-      Payload: JSON.stringify({
-        'Records': [
-          {
-            's3': {
-              'bucket': {
-                'name': bucket,
-              },
-              'object': {
-                'key': jsonData.pdf_path,
-              },
-            },
-          },
-        ],
-      }),
-    }
-
-    await lambda.invoke(lambdaParams).promise()
-
-    // Actualizo DynamoDB
-
-    // noinspection SpellCheckingInspection
-    const dynamodb = new AWS.DynamoDB({
-      apiVersion: '2012-08-10',
-      region: 'us-west-2',
-    });
-
-    let params = {
-      Item: {
-        "id": {
-          S: uuid4()
-        },
-        "expense": {
-          S: jsonData.expense,
-        },
-        "functional_unit_id": {
-          S: jsonData.functional_unit_id
-        },
-        "pdf_path": {
-          S: jsonData.pdf_path
-        },
-        "created_at": {
-          S: jsonData.created_at
-        },
-        "pdf_type": {
-          S: jsonData.pdf_type
-        },
-      },
-      TableName: JSON.parse(process.env.DDB)[bucket] // lo mando al tabla de ddb correspondiente
+    let ACQueueParams = {
+      MessageBody: message,
+      QueueUrl: ACQueueUrl,
+      DelaySeconds: 0,
     };
 
-    await dynamodb.putItem(params).promise();
+    await sqs.sendMessage(ACQueueParams).promise();
 
-    console.log(jsonData.pdf_path)
-    result = jsonData.pdf_path;
+    // Dejo el console.log para que quede registro en CloudWatch
+    console.log(jsonData.ruta);
+
+    result = jsonData.ruta;
 
     // Fin de la funcion
 
